c813ab0f032283295a7c46f4edb49729e7687121
John Larsson <john.larsson@scilifelab.se>
2016-09-27 11:44:29 +0200

    Switched to updated COG db

--- scripts/print_cog_db.py
+++ scripts/print_cog_db.py
@@ -3,14 +3,12 @@
 import urllib2, sys
 
 def main():
-    r = urllib2.urlopen("ftp://ftp.ncbi.nih.gov/pub/COG/COG/whog")
-    for line in r.readlines():
-        try: 
-            if line[0]=="[": 
-                line = line.rstrip()
-                items = line.rsplit()
-                print items[1]+"\t"+" ".join(items[2:])
-        except IndexError: continue
+    r = urllib2.urlopen("ftp://ftp.ncbi.nih.gov/pub/COG/COG2014/data/cognames2003-2014.tab")
+    for line in r.readlines(): 
+        if line[0]=="#": continue
+        line = line.rstrip()
+        items = line.split("\t")
+        print items[0]+"\t"+items[-1]
 
 if __name__ == '__main__':
     main()
