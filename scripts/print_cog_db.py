55a28c3c081ce5c9c1a4211d59e1e0db500e7b29
John Larsson <john.larsson@scilifelab.se>
2016-11-16 13:38:56 +0100

    Updated cog printing

--- scripts/print_cog_db.py
+++ scripts/print_cog_db.py
@@ -1,14 +1,16 @@
 #!/usr/bin/env python
 
-import urllib2, sys
+from urllib import request
+import sys
 
 def main():
-    r = urllib2.urlopen("ftp://ftp.ncbi.nih.gov/pub/COG/COG2014/data/cognames2003-2014.tab")
+    r = request.urlopen("ftp://ftp.ncbi.nih.gov/pub/COG/COG2014/data/cognames2003-2014.tab")
     for line in r.readlines(): 
+        line = line.decode('utf-8','ignore')
         if line[0]=="#": continue
         line = line.rstrip()
         items = line.split("\t")
-        print items[0]+"\t"+items[-1]
+        print(items[0]+"\t"+items[-1])
 
 if __name__ == '__main__':
     main()
